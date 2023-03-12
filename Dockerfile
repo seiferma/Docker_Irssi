c799b16c9797858ebb15ab6e9e1a6292c75781f4
Stephan Seifermann <seiferma@users.noreply.github.com>
2023-03-12 16:07:43 +0100

    support changing uid/gid

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ FROM quay.io/seiferma/tor-base:latest
 RUN http_proxy= && \
     https_proxy= && \
     apk add irssi ttyd abduco
-RUN adduser -h /home/irssi -D irssi
 
 ADD entrypoint-irssi.sh /entrypoint-irssi.sh
 
-VOLUME ["/home/irssi"]
+ENV DATA_VOLUME=/home/irssi-dckr
+VOLUME [${DATA_VOLUME}]
 EXPOSE 8080/tcp
 
 ENTRYPOINT ["/entrypoint-irssi.sh"]
--- entrypoint-irssi.sh
+++ entrypoint-irssi.sh
@@ -3,11 +3,27 @@
 set -e
 /entrypoint.sh true
 
-CMD=$1
+USER_NAME=irssi-dckr
+GROUP_NAME=irssi-dckr
 
-if [[ "$CMD" == "default" ]]; then
-	su-exec irssi ttyd -p 8080 abduco -a irssi &> /tmp/ttyd.log &
-	su-exec irssi abduco -c irssi torsocks irssi
+echo "Generating user account"
+if [ "$IRSSI_GID" == "" ]; then
+	export IRSSI_GID=1000
+fi
+if [ "$IRSSI_UID" == "" ]; then
+	export IRSSI_UID=1000
+fi
+addgroup -g $IRSSI_GID $GROUP_NAME
+adduser -D -H -G $GROUP_NAME -u $IRSSI_UID $USER_NAME
+mkdir -p $DATA_VOLUME
+chown $USER_NAME:$GROUP_NAME $DATA_VOLUME
+
+if [ "$1" == "default" ]; then
+        echo "Starting irssi"
+		su-exec $USER_NAME ttyd -p 8080 abduco -a irssi &> /tmp/ttyd.log &
+		nft add rule filter INPUT tcp dport 8080 ct state new accept
+		su-exec $USER_NAME abduco -c irssi torsocks irssi
 else
+	echo "Executing command"
 	exec "$@"
 fi
